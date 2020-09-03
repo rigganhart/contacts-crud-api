0cc1a3d330a72b8304962a2193ffd5bca9958d89
Riggan <riggan.hart@homes.com>
2020-09-03 10:21:58 -0400

    linting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const routes = require('./routes')
 const dbPlugin = require('./plugins/db')
 const sechemaPlugin = require('./plugins/schemas')
 const { plugin: sequencerPlugin } = require('./plugins/sequencer')
-const {host,port} = require('./config')
+const { host, port } = require('./config')
 const fastify = require('fastify')({
   logger: true
 })
@@ -15,7 +15,7 @@ routes.forEach(e => fastify.route(e))
 
 const start = async () => {
   try {
-    await fastify.listen(port,host)
+    await fastify.listen(port, host)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
